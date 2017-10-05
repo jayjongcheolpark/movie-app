93ca3899f78b47d0282134b367e3ce2f6df7f497
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-04 22:01:10 -0400

    Initial commit

---
+++ src/Movie.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        Movie
+      </div>
+    )
+  }
+}
+
+class MoviePoster extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Movie
