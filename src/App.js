84c2e03205c7b809a83ff3eae912a26709907de0
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-09 22:13:11 -0400

    Viewing movie card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import Movie from './Movie'
 
-
+// Render: componentWillMount() -> render() -> componentDidMount()
+// Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this._getMovies()
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    console.log(movies)
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return axios.get('https://yts.ag/api/v2/list_movies.json?sort_by=download_count')
+      .then(potato => potato.data.data.movies)
+      .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      )
+    })
+    return movies
+  }
+
   render() {
+    const { movies } = this.state
     return (
-      <div className="App">
-        <Movie />
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
